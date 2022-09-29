771434b6ca18a75945a5f49be388be340e2e6353
Digambaran <arjunsreekumar.1@gmail.com>
2022-09-29 19:55:10 +0530

    test

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-
-
 const block_for_fork = async (req, res) => {
-
   // health check
   if (req.params["health"] === "health") {
-    res.write(JSON.stringify({success: true, msg: "Health check success"}))
-    res.end()
+    res.write(JSON.stringify({ success: true, msg: "Health check success" }));
+    res.end();
   }
 
+  // This is a test
   // Add your code here
-  res.write(JSON.stringify({success: true, msg: `Hello block_for_fork`}))
-  res.end()
-  
-}
+  res.write(JSON.stringify({ success: true, msg: `Hello block_for_fork` }));
+  res.end();
+};
 
-export default block_for_fork
+export default block_for_fork;
